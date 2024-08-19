2fe77fb938f31464f4b87aaa17b2537ef282e207
yss930819 <yss930819@163.com>
2024-08-19 10:20:15 +0800

    feat: 增加使用库的文档地址链接

--- Watchlist/front-Umi/src/layouts/index.tsx
+++ Watchlist/front-Umi/src/layouts/index.tsx
@@ -1,43 +1,58 @@
 import { Outlet } from '@umijs/max';
-import { Button, Flex } from 'antd';
+import { App, Button, Flex } from 'antd';
 import React from 'react';
 import LOGO from '@/assets/black-cat.jpg';
 
 
 export default function() {
   return (
-    <Flex justify={'center'}>
-      <Flex gap={0} className={'tw-w-580px'} vertical>
-        <div className={'tw-flex tw-items-end tw-my-4'}>
-          <img src={LOGO} className={'tw-size-12 tw-mr-4'} />
-          <div className={'tw-text-2xl tw-font-bold'}>电影清单</div>
-        </div>
+    <App>
+      <Flex justify={'center'}>
+        <Flex gap={0} className={'tw-w-580px'} vertical>
+          <div className={'tw-flex tw-items-end tw-my-4'}>
+            <img src={LOGO} className={'tw-size-12 tw-mr-4'} />
+            <div className={'tw-text-2xl tw-font-bold'}>电影清单</div>
+          </div>
 
-        <Flex className={'tw-bg-black'} justify={'space-between'}>
-          <Flex justify={'flex-start'}>
-            <Button type={'link'} className={'tw-nav-link-button'}>首页</Button>
-            <Button type={'link'} className={'tw-nav-link-button'}>管理</Button>
+          <Flex className={'tw-bg-black'} justify={'space-between'}>
+            <Flex justify={'flex-start'}>
+              <Button type={'link'} className={'tw-nav-link-button'}>首页</Button>
+              <Button type={'link'} className={'tw-nav-link-button'}>管理</Button>
+            </Flex>
+            <Flex justify={'flex-end'}>
+              <Button type={'link'} className={'tw-nav-link-button'}>xxxx</Button>
+              <Button type={'link'} className={'tw-nav-link-button'}>登录</Button>
+              <Button type={'link'} className={'tw-nav-link-button'}>登出</Button>
+            </Flex>
           </Flex>
-          <Flex justify={'flex-end'}>
-            <Button type={'link'} className={'tw-nav-link-button'}>xxxx</Button>
-            <Button type={'link'} className={'tw-nav-link-button'}>登录</Button>
-            <Button type={'link'} className={'tw-nav-link-button'}>登出</Button>
+
+          <Outlet />
+
+          <Flex
+            vertical
+          >
+            <Flex gap={'small'}
+                  justify={'center'}
+                  align={'center'}>
+              <div className={'tw-footer-link-button'}>©2024</div>
+              <Button type={'link'} href={'https://github.com/yss930819'}
+                      className={'tw-footer-link-button'}>yss930819</Button>
+              <Button type={'link'} href={'https://github.com/yss930819/APIFlaskAndUmiLearn'}
+                      className={'tw-footer-link-button'}>APIFlaskAndUmiLearn </Button>
+              <Button type={'link'} href={'https://umijs.org/docs/introduce/introduce'}
+                      className={'tw-footer-link-button'}>Umi.js</Button>
+              <Button type={'link'} href={'https://ant-design.antgroup.com/index-cn'}
+                      className={'tw-footer-link-button'}>Antd</Button>
+              <Button type={'link'} href={'https://apiflask.com/'}
+                      className={'tw-footer-link-button'}>APIFlask</Button>
+              <Button type={'link'} href={'http://watchlist.helloflask.com/'}
+                      className={'tw-footer-link-button'}>Watchlist</Button>
+
+            </Flex>
+
           </Flex>
-        </Flex>
-
-        <Outlet />
-
-        <Flex
-          gap={'small'}
-          justify={'center'}
-          align={'center'}>
-          <div className={'tw-footer-link-button'}>©2024</div>
-          <Button type={'link'} href={'https://github.com/yss930819'}
-                  className={'tw-footer-link-button'}>yss930819</Button>
-          <Button type={'link'} href={'https://github.com/yss930819/APIFlaskAndUmiLearn'}
-                  className={'tw-footer-link-button'}>APIFlaskAndUmiLearn </Button>
         </Flex>
       </Flex>
-    </Flex>
+    </App>
   );
 }
